7ed420d710fe4d5beabe585555971a4a08c801fb
Kirill Vasiltsov <rensakamoto7@gmail.com>
2019-02-13 17:11:04 +0900

    Initial commit

---
+++ useFetch.js
@@ -0,0 +1,55 @@
+import { useState, useEffect, useRef } from "react";
+
+async function parseToJSON(response) {
+  return await response.json();
+}
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+function useFetch({
+  url,
+  delay,
+  options,
+  fetchFn = window.fetch,
+  parseFn = parseToJSON
+}) {
+  const [value, setValue] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function requestData() {
+    try {
+      const response = await fetchFn(url, options);
+      const value = await parseFn(response);
+      setValue(value);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => requestData(), []);
+
+  useInterval(() => requestData(), delay);
+
+  return { value, isLoading, error };
+}
+
+export default useFetch;
